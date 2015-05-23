619cb7f2499b3b5f6e98f421373902f712a7eff6
Sam Liu <wind13@163.com>
2015-05-23 21:19:43 +0800

    实现 slider 效果。

--- homepage.js
+++ homepage.js
@@ -1,4 +1,16 @@
-$(document)
-  .ready(function() {
-  })
-;
+$(function() {
+  $('#slides').slidesjs({
+    width: 1000,
+    height: 528,
+    navigation: {
+      active: false
+    },
+    play: {
+      active: false,
+      auto: true,
+      interval: 9000,
+      pauseOnHover: true,
+      swap: false
+    }
+  });
+});
